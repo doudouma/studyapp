7fcdddaccfcc408af63a5316e8292a0de37dc0a8
doudouma <ianni-k@163.com>
2026-09-19 16:48:37 +0800

    chore(appFooter): 更新Startup Fame的跳转链接
    为该链接添加上渠道追踪参数，便于统计流量来源

--- app/components/AppFooter.tsx
+++ app/components/AppFooter.tsx
@@ -114,7 +114,7 @@ export function AppFooter() {
           label="Good AI Tools"
         />
         <BadgeLink
-          href="https://startupfa.me"
+          href="https://startupfa.me/s/100mini?utm_source=100mini.com"
           imgSrc="https://startupfa.me/badges/featured-badge-small.webp"
           label="Startup Fame"
         />
